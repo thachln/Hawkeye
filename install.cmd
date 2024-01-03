e8cee32048fbb8d12a721a10103634c14f79950d
GLOMQuyet <deeplearningglom0@gmail.com>
2024-01-03 15:37:15 +0700

    Script để hỗ trợ chuẩn bị môi trường

---
+++ install.cmd
@@ -0,0 +1,9 @@
+rem Tạo biến môi trường
+python -m venv .env
+rem kích hoạt biến môi trường
+.env\Scripts\activate
+rem Requirements
+pip install numpy
+pip install tqdm
+pip install yac
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
