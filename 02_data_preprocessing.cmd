bbacab4da19d6e04b1a4e914bbc94b3a9f49c744
Thach.Le <thachln@mks.com.vn>
2024-02-05 15:38:00 +0700

    Update scripts.

--- 02_data_preprocessing.cmd
+++ 02_data_preprocessing.cmd
@@ -1,13 +1,6 @@
 @ECHO OFF
 rem Prepare data/dataset by uncompress file data/dataset.zip into data/dataset
 
-rem Activate the virtual environment
-call venv\Scripts\activate
-rem update pip
-python -m pip install --upgrade pip
-
 rem Perform data augmentation
 python dataset/data_augmentation.py
-rem Generate metadata: train.txt, val.txt 
-python dataset/metadata.py
 
